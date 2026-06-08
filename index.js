55f6c54d6e571072e03c94a4f17c37bf864a1291
Lucasmpmof <lopesantoniolucas@gmail.com>
2026-06-08 12:02:08 -0300

    Change color when btn clicked

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 document.getElementById("title").innerText =
   "hello world, changed by javascript";
+
+btn = dcument.getElementById("btn").innerText = "";
+
+function Change() {
+  console.log("Foi");
+  document.getElementById("espaço").style.cssText = "background-color: red";
+}
+cor = document.getElementById("coloração");
+console.log(cor);
