a17d2acfc13634cc16be62b1294e99a59f3fd4d2
Lucasmpmof <lopesantoniolucas@gmail.com>
2026-06-09 12:07:19 -0300

    Mudanças index e javascript

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-document.getElementById("title").innerText =
-  "hello world, changed by javascript";
+let num = document.getElementById("numero");
+let count = 0;
 
-btn = dcument.getElementById("btn").innerText = "";
-
+let num_salvo1 = document.getElementById("num1");
+let num_salvo2 = document.getElementById("num2");
+let contador_num = 0;
 function Change() {
-  console.log("Foi");
-  document.getElementById("espaço").style.cssText = "background-color: red";
+  count = count + 1;
+  num.innerText = count;
 }
-cor = document.getElementById("coloração");
-console.log(cor);
+
+function Resetar() {
+  count = 0;
+  num.innerText = count;
+}
+
+function Salvar() {
+  if (contador_num == 0) {
+    num_salvo1.innerText = count;
+    count = 0;
+    contador_num = +1;
+  }
+  if (contador_num == 1) {
+    num_salvo2.innerText = count;
+    count = 0;
+    contador_num = -1;
+    console.log(count);
+  }
+}
